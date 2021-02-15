034050641d960ce1fbcc396586dbc4b16d46100e
Gany Gemilang <64389068+GanyGemilang@users.noreply.github.com>
2021-02-15 17:59:13 +0700

    ADD MODULE Request, RequestHistory, Parameter
    CRUD All Module, Realasi, and LazyLoading

---
+++ LeaveRequest/Controllers/RequestHistoryController.cs
@@ -0,0 +1,22 @@
+﻿using LeaveRequest.Base.Controller;
+using LeaveRequest.Models;
+using LeaveRequest.Repositories.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveRequest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestHistoryController : BaseController<RequestHistory, RequestHistoryRepository>
+    {
+        public RequestHistoryController(RequestHistoryRepository requestHistoryRepository) : base(requestHistoryRepository)
+        {
+
+        }
+    }
+}
